60d6dea306ead1f2ae96d5c207d645095670ecd6
ARTHURpvn <arthursppavan@gmail.com>
2025-05-18 17:54:30 -0300

    feat: making the SEO

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <div className="bg-card flex h-16 items-center justify-center px-4 md:h-20 md:px-6">
+    <div className="bg-card flex h-16 items-center justify-center px-4 mt-6 md:h-20 md:px-6">
       <p className="text-center text-base font-semibold md:text-lg">
       {t("footer.copyright")} <span className="text-primary font-bold"> arthurpvn.dev </span>{" "} {t("footer.rights")}
         
